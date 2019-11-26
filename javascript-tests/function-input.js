1371e5ea84a685a2b5ae0f3ade71b24f76c95106
Sophie Conroy <liquidsky.co@gmail.com>
2019-11-26 14:59:17 -0800

    modified on 11/26

--- javascript-tests/function-input.js
+++ javascript-tests/function-input.js
@@ -1,72 +1,52 @@
 window.onload = () => {
-    // All code goes in here.
-    const feetToInches = document.getElementById('feetToInchesAction');
-    const milesToFeet = document.getElementById('milesToFeetAction');
-    const areaOfCircle = document.getElementById('areaOfCircleAction')
-    const areaOfTriangle = document.getElementById('areaOfTriangleAction');
-    const feetToInchesDisplay = document.getElementById('feetToInchesDisplay');
-    const milesToFeetDisplay = document.getElementById('milesToFeetDisplay');
-    const areaOfCircleDispaly = document.getElementById('areaOfCircleDisplay')
-    const areaOfTriangleDisplay = document.getElementById('areaOfTriangleDisplay');
-    const addToListAction = document.getElementById('addToListAction');
-    const nameDisplay = document.getElementById('nameDisplay').value;
-    const listDisplay = document.getElementById('listDisplay');
-
+   
+    const feetToInchesAction = document.getElementById('feetToInchesAction');
+    const milesToFeetAction = document.getElementById('milesToFeetAction');
+    const areaOfCircleAction = document.getElementById('areaOfCircleAction');
+    const areaOfTriangleAction = document.getElementById('areaOfTriangleAction');
+    
     feetToInchesAction.onclick = () => {
+        
         const feetToInchesInput = document.getElementById('feetToInchesInput');
-        const feetToInchesDisplay = document.getElementById('feetToInchesDisplay');
+        const feetToInches = (feet) => {
+            return feet * 12;
+        };
+ 
+        document.getElementById("feetToInchesDisplay").innerHTML= feetToInches(feetToInchesInput.value);
+    };
 
-        feetToInchesDisplay.textContent = feetToInches(feetToInchesInput.value);
+    milesToFeetAction.onclick = () => {
+        
+        const milesToFeetInput = document.getElementById('milesToFeetInput');
+        const milesToFeet = (miles) => {
+            return miles * 5280;
+        };
+ 
+        document.getElementById("milesToFeetDisplay").innerHTML= milesToFeet(milesToFeetInput.value);
+    };
+
+    areaOfCircleAction.onclick = () => {
+        
+        const areaOfCircleInput = document.getElementById('areaOfCircleInput');
+        const areaOfCircle = (radius) => {
+            return Math.PI * (radius * radius);
+        };
+ 
+        document.getElementById("areaOfCircleDisplay").innerHTML= areaOfCircle(areaOfCircleInput.value);
+
+    };
+
+    areaOfTriangleAction.onclick = () => {
+        
+        const areaOfTriangleBaseInput = document.getElementById('areaOfTriangleBaseInput');
+        const areaOfTriangleHeightInput = document.getElementById('areaOfTriangleHeightInput');
+        const areaOfTriangle = (base, height) => {
+            return (base * height)/2;
+        };
+ 
+        document.getElementById("areaOfTriangleDisplay").innerHTML= areaOfTriangle(areaOfTriangleBaseInput.value,areaOfTriangleHeightInput.value);
+
     };
 };
 
-const feetToInches = (feet) => {
-    return feet * 12;
-};
-                    
-
-addToListAction.onclick = function(){
-    const nameDisplay = document.getElementById('nameDisplay').value;
-    const listDisplay = document.getElementById('listDisplay');
-    addlistFunction(listDisplay,nameDisplay);
-
-}
-
-
-function addlistFunction(){
-    const nameDisplay = document.getElementById('nameDisplay').value;
-
-    console.log("Add item to list.");
-    addToListFunctionDisplay.textContent = nameDisplay;
-}
-
-
-
-
-
-function functionParameters(value){
-    console.log(value);
-}
-
-function functionReturn(addResult){
-    return "The add function returned" + addResult;
-}
-
-function add (a, b){
-    const addResult = a + b;
-    console.log('Inside add', addResult);
-    return addResult;
-}
-
-
-functionParameters("Many functions take parameters.");
-
-const addResult = add(2, 3);
-
-const returnValue = functionReturn(addResult);
-
-console.log(returnValue);
-
-const result = add(2, 3);
-console.log(result);
 
