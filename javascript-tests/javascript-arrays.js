da2a054094d590decc2bb2dc00ecb9d568780843
Sophie Conroy <liquidsky.co@gmail.com>
2019-11-29 16:57:35 -0800

    modified arrays

--- javascript-tests/javascript-arrays.js
+++ javascript-tests/javascript-arrays.js
@@ -28,7 +28,7 @@ window.onload = () => {
     const languageDisplay = document.getElementById('language-display');
 
     const languagearrays = {
-        language: ["JavaScript","HTML","CSS","C#","Java","C#","Python","C/C++"]
+        language: ["JavaScript","HTML","CSS","C#","Java","Python","C/C++"]
     };
 
     languagearrays.language.sort();
@@ -37,6 +37,63 @@ window.onload = () => {
         for (let language of languagearrays.language){
         console.log(language);
         elfCode.appendToList(languageDisplay, language);
+         }
+    }
+
+    var programinglanguage = [
+        {
+            language: 'JavaScript',
+            rank: 2
+        },
+        {
+            language: 'HTML/CSS',
+            rank: 1
+        },
+        {
+            languageName: 'Python',
+            rank: 3
+        },
+        {
+            languageName: 'Java',
+            rank: 4
+        },
+        {
+            languageName: 'C#',
+            rank: 5
+        },
+        {
+            languageName: 'C/C++',
+            rank: 6
+        }
+    ];
+
+    const popularityAction = document.getElementById('popularity-action');
+    const popularityDisplay = document.getElementById('popularity-display');
+
+    const popularityarrays = {
+        language: ["JavaScript","HTML/CSS","Python","Java","C#","C/C++"]
+    };
+
+    function compareRank(a, b) {
+        const rankA = a.rank;
+        const rankB = b.rank;
+   
+       let comparison = 0;
+       if (rankA > rankB ) {
+           comparison = 1;
+       } else if (rankA < rankB) {
+           comparison = -1;
+       }
+       return comparison;
+   }
+    
+    programinglanguage.sort(compareRank);
+    /* popularityarrays.popularity.sort(compareRank); */
+
+    languageAction.onclick = () => { 
+        for (let language of popularityarrays.language){
+        console.log(popularity);
+        elfCode.appendToList(popularityDisplay, language);
          }
     }
 
