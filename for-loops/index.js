8c8c0860b6ff5d0f80d819e4ed66eee5d7700ee9
Sophie Conroy <liquidsky.co@gmail.com>
2019-12-03 18:25:30 -0800

    modified loops

--- for-loops/index.js
+++ for-loops/index.js
@@ -1,4 +1,3 @@
-window.onload = () => {
     const elfCode = {
         appendToList: (list, value) => {
             const li = document.createElement("li");
@@ -6,16 +5,18 @@ window.onload = () => {
             list.appendChild(li);
         }
     };
-
-    const listButton = document.getElementById('runForLoop');
-
-    listButton.onclick = () => {
-        const myList = document.getElementById('myList');
-            
-            elfCode.appendToList(myList, 1);
-            elfCode.appendToList(myList, 2);
-            elfCode.appendToList(myList, 3);
-            
+    
+    window.onload = () => {
+    
+        const listButton = document.getElementById('runForLoop');
+    
+        listButton.onclick = () => {
+            const myList = document.getElementById('myList');
+            for (var i = 0; i < 5; i++){
+                elfCode.appendToList(myList, i);
+            }
+        }
     }
-}                    
+   
+                   
         
